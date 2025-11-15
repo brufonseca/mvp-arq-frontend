105346acb0c69c2a5965d8d4b1e762b1b8ea102a
Bruna Fonseca <brunafonseca@gmail.com>
2025-11-15 12:20:55 -0300

    fix: icon button fill color

--- src/components/journal-form.js
+++ src/components/journal-form.js
@@ -13,7 +13,9 @@ export class JournalForm extends LitElement {
             :host {
                 display: block;
                 font-family: "Roboto", sans-serif;
+                
             }
+
 
             .form-title{
                 padding:8px;
@@ -52,13 +54,13 @@ export class JournalForm extends LitElement {
                 <h3>Novo Registro</h3>
                 <div class="left-container">
 
-                    <md-filled-tonal-icon-button>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>content-save</title><path d="M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z" /></svg>
-                    </md-filled-tonal-icon-button>
+                    <md-filled-icon-button>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>content-save</title><path d="M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z" fill="#ffffff" /></svg>
+                    </md-filled-icon-button>
 
-                    <md-filled-tonal-icon-button>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>
-                    </md-filled-tonal-icon-button>
+                    <md-filled-icon-button>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" fill="#ffffff"/></svg>
+                    </md-filled-icon-button>
 
                 </div>
 
