c3212a1659f7518a65f8aa71048adfe537b4c172
Bruna Fonseca <brunafonseca@gmail.com>
2025-11-19 20:31:06 -0300

    feat: add radio buttons

--- src/components/form-meal-item.js
+++ src/components/form-meal-item.js
@@ -1,6 +1,33 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 
-import "@material/web/checkbox/checkbox.js";
+import "@material/web/radio/radio.js";
+import "@material/web/iconbutton/filled-icon-button.js";
+
+const MEAL_TYPE = {
+    morningSnack: "LANCHE_MANHA",
+    lunch: "ALMOCO",
+    afternoonSnack: "LANCHE_TARDE",
+    dinner: "JANTAR",
+};
+
+const MEAL_METHOD = [
+    { type: "BLW", label: "BLW" },
+    { type: "PARTICIPATIVO", label: "Participativo" },
+    { type: "TRADICIONAL", label: "Tradicional" },
+];
+
+const MEAL_ACCEPTANCE = [
+    { type: "OTIMO", label: "Ótimo" },
+    { type: "BOM", label: "Bom" },
+    { type: "REGULAR", label: "Regular" },
+    { type: "RECUSOU", label: "Recusou" },
+];
+
+const MEAL_RATING = [
+    { type: "SUCESSO", label: "Sucesso" },
+    { type: "OK", label: "Bom" },
+    { type: "CAOS", label: "Caos" },
+];
 
 export class FormMealItem extends LitElement {
     static styles = [
@@ -8,7 +35,7 @@ export class FormMealItem extends LitElement {
             :host {
                 display: block;
             }
-        `
+        `,
     ];
 
     static properties = {
@@ -17,13 +44,20 @@ export class FormMealItem extends LitElement {
 
     render() {
         return html`
-            <div>${this.meal}</div>
+            <h4>Método</h4>
+            ${MEAL_METHOD.map(item => html`
+            <div> 
+                <md-radio  id="${item.type}" name="metodo" value="${item.type}"></md-radio>
+                <label for="${item.type}">${item.label}</label>
+            </div>
+          `)}
 
-            <label>
-                <md-checkbox touch-target="wrapper"></md-checkbox>
-                Checkbox one
-            </label>
+            <h4>Aceitação</h4>
+
+            <h4>Avaliação Geral</h4>
+
+            <h4>Comentários</h4>
         `;
     }
 }
-customElements.define('form-meal-item', FormMealItem);
+customElements.define("form-meal-item", FormMealItem);
